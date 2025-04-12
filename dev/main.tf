93bfe2ab6b74c7c1ac8e1717676ef4041570f653
Fiacre Ncho <f_ncho@me.com>
2025-04-12 17:19:42 -0500

    removed details in dev folders

--- dev/main.tf
+++
@@ -1,5 +0,0 @@
-# main.tf (or any .tf file in your root module)
-module "ec2_instance" {
-  source = "../modules/ec2"  # Path to your module
-  config = var.config       # Values auto-loaded from terraform.tfvars
-}
